cff9a63959d47f506142501bbe77231d7bc02b1e
Anse14 <andre.segovia@utec.edu.pe>
2021-12-09 20:25:39 -0500

    Created Project

---
+++ src/test/java/com/utec/lab/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.utec.lab;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void callMeMaybe() {
+        Assert.assertEquals(1, 1);
+    }
+}
